271c869ced369ff3b60585df8f0991b5873069bd
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-03-06 16:14:29 +0530

    Add files via upload

---
+++ Palindrome.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    private:
+    public:
+        Solution();
+        int palindromeOrNot(string str);
+};
+Solution::Solution(){}
+static bool isPalin(string::iterator low, string::iterator high)
+{
+    while(low < high)
+    {
+        if(*low != *high)
+        {
+            return false;
+        }
+        low++;
+        high--;
+    }
+    return true;
+}
+int Solution::palindromeOrNot(string str)
+{
+    int low = 0;
+    int high = str.length()-1;
+    if(isPalin(str.begin()+low, str.begin()+high))
+    {
+        return 1;
+    }
+    return 0;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution sol;
+    int testCase;
+    cin >> testCase;
+    while(testCase--)
+    {
+        string str;
+        cin >> str;
+        int ans = sol.palindromeOrNot(str);
+        if(ans == 1)
+        {
+            cout << "YES" <<endl;
+        }
+        else
+        {
+            cout << "NO" <<endl;
+        }
+    }
+    return 0;
+}
